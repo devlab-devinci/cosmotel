3ac019616fb11b97970feb66f7d222f9a97d7306
Antoine Piché <antoinepiche@gmail.com>
2019-01-29 15:47:10 +0100

    add: misc files / fixes

--- public/js/app.js
+++ public/js/app.js
@@ -13699,63 +13699,46 @@ module.exports = __webpack_require__(36);
 /***/ (function(module, exports, __webpack_require__) {
 
 
+__webpack_require__(12);
 /**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
+ * Main JS file.
+ * Please keep it clean.
  */
-
-__webpack_require__(12); // NOTE: still used ? 
-
 
 var maxHeightRestaurant = 0;
 var $restaurant = $('.restaurant');
 var $gor = {
-    name: $('.GORName'),
-    description: $('.GORDescription')
-    // informations :
+  name: $('.GORName'),
+  description: $('.GORDescription')
+  // informations :
 };
 $(document).ready(function () {
 
-    if ($restaurant.length > 0) {
-        $.each($restaurant, function (index) {
-            if ($(this).height() > maxHeightRestaurant) {
-                maxHeightRestaurant = $(this).height();
-            }
-        });
-
-        $restaurant.children().height(maxHeightRestaurant);
-    }
-
-    $restaurant.click(function () {
-        // var data = {'id': $(this).attr('data-id')};
-        // console.log(data);
-        /*$.ajax({
-            url: url,
-            dataType: 'json',
-            data: data,
-            type: 'POST',
-            error: function (jqXHR, status, errorCode) {
-                console.log(status);
-            }
-        }).done(function (response, status, jqXHR) {
-            console.log(response);
-        });*/
-
-        var urlToUse = url + "/restaurant/getOne/" + $(this).attr('data-id');
-        console.log(url);
-        $.ajax({
-            url: urlToUse,
-            type: 'GET',
-            error: function error(jqXHR, status, errorCode) {
-                console.log(status);
-            }
-        }).done(function (response, status, jqXHR) {
-            $gor.name.html(response.restaurant.name);
-            $gor.description.html(response.restaurant.description);
-            // $gor.informations.restaurateur.html(response.)
-        });
+  if ($restaurant.length > 0) {
+    $.each($restaurant, function (index) {
+      if ($(this).height() > maxHeightRestaurant) {
+        maxHeightRestaurant = $(this).height();
+      }
     });
+
+    $restaurant.children().height(maxHeightRestaurant);
+  }
+
+  $restaurant.click(function () {
+    var urlToUse = url + "/restaurant/getOne/" + $(this).attr('data-id');
+    console.log(url);
+    $.ajax({
+      url: urlToUse,
+      type: 'GET',
+      error: function error(jqXHR, status, errorCode) {
+        console.log(status);
+      }
+    }).done(function (response, status, jqXHR) {
+      $gor.name.html(response.restaurant.name);
+      $gor.description.html(response.restaurant.description);
+      // $gor.informations.restaurateur.html(response.)
+    });
+  });
 });
 
 /***/ }),
