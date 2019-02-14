105d3ed0b17d6300a4466de820dcf735cb07e063
SimonDebray <debray.c.simon@gmail.com>
2019-02-14 12:57:48 +0100

    fix indent

--- resources/views/influencer/restaurant/list.blade.php
+++ resources/views/influencer/restaurant/list.blade.php
@@ -1,16 +1,16 @@
 @if (!empty($restaurants))
-        @foreach($restaurants as $restaurant)
-            <div class="col-sm-4 col-xs-6 col-12 restaurant" data-id="{{ $restaurant->id }}">
-                <a href="{{ route('influencer::restaurant.show', $restaurant->id) }}">
-                    <div class="card">
-                        <div class="card-header">
-                            {{ $restaurant->name }}
-                        </div>
-                        <div class="card-body">
-                            {{ $restaurant->description }}
-                        </div>
+    @foreach($restaurants as $restaurant)
+        <div class="col-sm-4 col-xs-6 col-12 restaurant" data-id="{{ $restaurant->id }}">
+            <a href="{{ route('influencer::restaurant.show', $restaurant->id) }}">
+                <div class="card">
+                    <div class="card-header">
+                        {{ $restaurant->name }}
                     </div>
-                </a>
-            </div>
-        @endforeach
+                    <div class="card-body">
+                        {{ $restaurant->description }}
+                    </div>
+                </div>
+            </a>
+        </div>
+    @endforeach
 @endif
